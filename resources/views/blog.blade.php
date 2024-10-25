fa5c7a29732ff7822c98e26fb39b10cc76e4739f
AzharLuthfi <azhar.luthfiadi@gmail.com>
2024-10-25 09:42:35 +0700

    Video 2 Laravel 11 - Strukur Folder

---
+++ resources/views/blog.blade.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Halaman About</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+    <a href="/">Home</a>
+    <a href="/about">About</a>
+    <a href="/blog">Blog</a>
+    <a href="/contact">Contact</a>
+    <h1>Halaman Blog</h1>
+    <hr>
+    
+    <article>
+        <h2>Sentra Kuliner Bandung</h2>
+        <p>
+            Bandung merupakan sentral kuliner modern. Banyak inovasi-inovasi baru dalam pengolahan makanan.
+            bukan haya itu, makanan jadul seperti cilok, somay, batagor sudah mengalami perkembangan 
+        </p>
+    </article>
+    <br>
+    <article>
+        <h2>Keseharian ku </h2>
+        <p>
+            Hari ini ku bangun dengan semangat yang entah dari mana asalnya. ku fikir bisa menelesaikan
+            semua tugas tanpa masalah.
+        </p>
+    </article>
+    
+</body>
+</html>
