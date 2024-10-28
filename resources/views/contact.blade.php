8ee5e698682f44154b5729f1f53ad86620c6a1c9
AzharLuthfi <azhar.luthfiadi@gmail.com>
2024-10-28 17:32:43 +0700

    Video 5 Laravel11 - Blade Component

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -1,18 +1,9 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="css/style.css">
-    <title>Halaman Contact</title>
-</head>
-<body>
-    <a href="/">Home</a>
-    <a href="/about">About</a>
-    <a href="/blog">Blog</a>
-    <a href="/contact">Contact</a>
-    <h1>Halaman Contact</h1>
+
+<x-Layout>
+
+    <x-slot:title>{{ $title }}</x-slot:title>
+    <h3 class="text-xl">Ini adalah halaman Contact!</h3>
+    
     <ul>
         <li display="none">
             <a href="https://instagram.com/azhr_luth">Instagram</a>
@@ -21,7 +12,5 @@
             <a href="https://youtube.com/MuhamadAzharLuthfiadi">Youtube</a>
         </li>
     </ul>
-    
-    
-</body>
-</html>
+  
+  </x-Layout>
